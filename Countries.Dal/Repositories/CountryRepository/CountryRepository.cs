f80bfb64f80cf7c786379de7d42891a0fad9a990
MHudiakov <MHudiakov@init-center.ru>
2017-06-15 11:09:26 +0300

    Add repos

---
+++ Countries.Dal/Repositories/CountryRepository/CountryRepository.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Countries.Dal.Models.Country;
+
+namespace Countries.Dal.Repositories.CountryRepository
+{
+    /// <summary>
+    /// Репозиторий стран
+    /// </summary>
+    public class CountryRepository : ICountryRepository
+    {
+        /// <summary>
+        /// Получить всю коллекцию стран
+        /// </summary>
+        public IList<ICountry> CountryCollection { get; set; }
+
+        /// <summary>
+        /// Получить коллекцию стран по части имени столицы
+        /// </summary>
+        /// <param name="pattern">
+        /// Паттерн поиска
+        /// </param>
+        /// <returns>
+        /// Коллекция стран
+        /// </returns>
+        public IList<ICountry> GetCountryCollectionByCapital(string pattern)
+        {
+            IList<ICountry> countries = CountryCollection;
+
+            IList<ICountry> result = countries.Where(country => country.Capital.ToLower().Contains(pattern.ToLower()))
+                .ToList();
+
+            return result;
+        }
+
+        /// <summary>
+        /// Получить коллекцию стран по части названия
+        /// </summary>
+        /// <param name="pattern">
+        /// Паттерн поиска
+        /// </param>
+        /// <returns>
+        /// Коллекция стран
+        /// </returns>
+        public IList<ICountry> GetCountryCollectionByName(string pattern)
+        {
+            IList<ICountry> countries = CountryCollection;
+
+            IList<ICountry> result = countries.Where(country => country.Name.ToLower().Contains(pattern.ToLower()))
+                .ToList();
+
+            return result;
+        }
+    }
+}
---
+++ Countries.Dal/Repositories/SettingsRepository/SettingsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Countries.Dal.Models.Settings;
+
+namespace Countries.Dal.Repositories.SettingsRepository
+{
+    /// <summary>
+    /// Репозиторий настроек
+    /// </summary>
+    public class SettingsRepository : ISettingsRepository
+    {
+        /// <summary>
+        /// Настройки приложения
+        /// </summary>
+        public ISettings Settings { get; set; }
+    }
+}
