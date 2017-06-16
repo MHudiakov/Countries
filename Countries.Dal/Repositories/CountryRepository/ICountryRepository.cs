f8af75ab34f77c5edd938f96fbd675c7fdb25a9b
MHudiakov <MHudiakov@init-center.ru>
2017-06-16 12:35:45 +0300

    Убрана фильтрация из репозиториев

--- Countries.Dal/Repositories/CountryRepository/ICountryRepository.cs
+++ Countries.Dal/Repositories/CountryRepository/ICountryRepository.cs
@@ -12,27 +12,5 @@ namespace Countries.Dal.Repositories.CountryRepository
         /// Коллекция стран
         /// </summary>
         IList<ICountry> CountryCollection { get; set; }
-
-        /// <summary>
-        /// Получить коллекцию стран по части имени столицы
-        /// </summary>
-        /// <param name="pattern">
-        /// Паттерн поиска
-        /// </param>
-        /// <returns>
-        /// Коллекция стран
-        /// </returns>
-        IList<ICountry> GetCountryCollectionByCapital(string pattern);
-
-        /// <summary>
-        /// Получить коллекцию стран по части названия
-        /// </summary>
-        /// <param name="pattern">
-        /// Паттерн поиска
-        /// </param>
-        /// <returns>
-        /// Коллекция стран
-        /// </returns>
-        IList<ICountry> GetCountryCollectionByName(string pattern);
     }
 }
